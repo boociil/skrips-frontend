118f0958874bfb2c156d886966d7f0af3cbbe2f1
gggemming <222011334@stis.ac.id>
2024-05-22 16:43:34 +0700

    do:idk

--- src/pages/RekapWithID.jsx
+++ src/pages/RekapWithID.jsx
@@ -315,7 +315,7 @@ const RekapWithID = () => {
                             
 
                         </div>
-
+                        
                     </div>
                 )}            
             </div>
